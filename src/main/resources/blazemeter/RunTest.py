84e7e4e59a1643f8dc9ac1b9199fec626e54b4b1
Jaco Greyling <jgreyling@gmail.com>
2018-05-21 15:42:39 +0200

    Fixed error with return object incorrectly treated as array

--- src/main/resources/blazemeter/RunTest.py
+++ src/main/resources/blazemeter/RunTest.py
@@ -23,8 +23,8 @@ def callUrl(verb, url):
         elif verb == 'get':
             request = urllib2.Request(url)
         else:
-            print 'HTTP verb error!'
-            print 'Reason: Only POST and GET verbs supported.'
+            print 'HTTP verb error!\n'
+            print 'Reason: Only POST and GET verbs supported.\n'
             sys.exit(201)
 
         request.add_header("Authorization", "Basic %s" % base64string)            
@@ -34,20 +34,20 @@ def callUrl(verb, url):
         return output
     # Catch all exceptions
     except urllib2.HTTPError as error:
-        print 'HTTP %s error!' % error.code
-        print 'Reason: %s (URL: %s)' % (error.msg, url)
+        print 'HTTP %s error!\n' % error.code
+        print 'Reason: %s (URL: %s)\n' % (error.msg, url)
         sys.exit(202)
     except urllib2.URLError as error:
-        print 'Network error!'
-        print 'Reason: %s' % error.reason
+        print 'Network error!\n'
+        print 'Reason: %s\n' % error.reason
         sys.exit(203)
     except ValueError as error:
-        print 'JSON parsing error!'
-        print 'Reason: %s' % error.message
+        print 'JSON parsing error!\n'
+        print 'Reason: %s\n' % error.message
         sys.exit(204)
     except Exception as error:
-        print 'Uncaught error!'
-        print 'Reason: %s' % error
+        print 'Uncaught error!\n'
+        print 'Reason: %s\n' % error
         sys.exit(205)
 
 # Initialize variables
@@ -60,27 +60,27 @@ account = ''
 
 # Make sure all the required paramaters are set
 if not base_url.strip():
-    print 'Input error!'
+    print 'Input error!\n'
     print 'Reason: Server configuration url undefined\n'
     sys.exit(101)
 
 if not keyId.strip():
-    print 'Input error!'
+    print 'Input error!\n'
     print 'Reason: Parameter keyId undefined\n'
     sys.exit(102)
 
 if not secret.strip():
-    print 'Input error!'
+    print 'Input error!\n'
     print 'Reason: Parameter secret undefined\n'
     sys.exit(103)
 
 if not test.strip():
-    print 'Input error!'
+    print 'Input error!\n'
     print 'Reason: Parameter test undefined\n'
     sys.exit(104)
 
 if not pollingInterval:
-    print 'Input error!'
+    print 'Input error!\n'
     print 'Reason: Parameter pollingInterval undefined\n'
     sys.exit(104)
 
@@ -91,15 +91,17 @@ data = callUrl('post', start_test_url)
 session = data.get('result').get('sessionsId')[0]
 
 # Monitor the progress of the session
+print 'The following session id has been created for this test run: %s\n' % session
 session_status_url = '%s/sessions/%s/status' % (base_url, session)
 count = 1
 while True:
-    print 'Checking to see if the test is complete: checking #%d\n' % count
+    print 'Checking to see if the test is complete: retry #%d\n' % count
     data = callUrl('get', session_status_url)
     if data.get('result').get('status') == "ENDED":
         if 'errors' in data.get('result') and data.get('result').get('errors'):
-            print 'Session %s error!' % data.get('result').get('errors').get('code')
-            print 'Reason: %s' % data.get('result').get('errors').get('message')
+            error = data.get('result').get('errors')[0]
+            print 'Session %s error!\n' % error.get('code')
+            print 'Reason: %s\n' % error.get('message')
             sys.exit(301)
         else:
             break
@@ -127,14 +129,14 @@ if result and 'id' in result:
 master_url = '%s/masters/%s' % (base_url, master)
 data = callUrl('get', master_url)
 if 'passed' in data.get('result') and data.get('result').get('passed') == False:
-    print 'BlazeMeter test %s failed!' % test
+    print 'BlazeMeter test %s failed!\n' % test
     print '```'
     print json.dumps(data)
     print '```'
     sys.exit(1)
 
 # Print output
-print 'BlazeMeter test %s completed successfully' % test
+print 'BlazeMeter test %s completed successfully\n' % test
 print '```'
 print json.dumps(data)
 print '```'
